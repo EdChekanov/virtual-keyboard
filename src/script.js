6bef47f0da398d9a9380210ff969dcf5e7c2710c
Ed <kide.da@gmail.com>
2023-04-30 14:54:40 +0300

    feat: add datafile with keybord btns

--- src/script.js
+++ src/script.js
@@ -1,2 +1,8 @@
 import './index.html';
 import './style.scss';
+
+import data from './keyboard.json';
+
+document.addEventListener('keydown', (e) => {
+  for (let key in data) console.log(data[key]);
+});
