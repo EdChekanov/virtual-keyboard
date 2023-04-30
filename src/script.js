caa357a63cda76e7a78278e2166568b8b4052e64
Ed <kide.da@gmail.com>
2023-04-30 15:01:05 +0300

    feat: add eslint + airbnb config

--- src/script.js
+++ src/script.js
@@ -1,8 +1,4 @@
 import './index.html';
 import './style.scss';
 
-import data from './keyboard.json';
-
-document.addEventListener('keydown', (e) => {
-  for (let key in data) console.log(data[key]);
-});
+// import data from './keyboard.json';
